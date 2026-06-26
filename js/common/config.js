1d9fb10d7f29af099b896a6feb5f81da729281f6
rsigor-stack <rsigor1040@gmail.com>
2026-06-26 14:49:13 -0300

    Update config.js

--- js/common/config.js
+++ js/common/config.js
@@ -1,1 +1,23 @@
+//======================================================
+// CONFIGURAÇÃO GLOBAL DO SITE
+//======================================================
 
+// Apps Script principal
+
+const URL_BASE =
+"https://script.google.com/macros/s/AKfycbyr_jCKmx6ij0fytIqNjK4evsorDQtxdp4bcvnJ4RQMvU6TTK9zwJZZXjm__p3uyKI/exec";
+
+// Pasta de imagens
+
+const URL_IMAGENS =
+"/imagens/";
+
+// CDN das bandeiras
+
+const URL_FLAGCDN =
+"https://flagcdn.com";
+
+// Versão do sistema
+
+const APP_VERSION =
+"1.0.0";
